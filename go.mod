2df1a65a91a7d4a70406a378e4ef00a76fc8763a
sLm <185361376+sLm-acs@users.noreply.github.com>
2025-07-23 20:44:01 +0200

    docker-compose psql

--- go.mod
+++ go.mod
@@ -1,8 +1,18 @@
 module github.com/slmkb/weblensgo
 
-go 1.22.1
+go 1.23.0
+
+toolchain go1.23.11
 
 require (
 	github.com/go-chi/chi v1.5.5
 	github.com/go-chi/chi/v5 v5.2.2
+)
+
+require (
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
+	github.com/jackc/pgx/v5 v5.7.5 // indirect
+	golang.org/x/crypto v0.37.0 // indirect
+	golang.org/x/text v0.24.0 // indirect
 )
--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 func main() {
 	r := chi.NewRouter()
 
-	homeTpl := views.Must(views.Parse(filepath.Join("templates", "home.gohtml")))
+	homeTpl := views.Must(views.Parse("templates/base.gohtml", "templates/home.gohtml"))
 	r.With(middleware.Logger).Get("/", controllers.StaticHandler(homeTpl))
 
-	contactTpl := views.Must(views.Parse("templates/base.gohtml", "templates/contact.gohtml"))
+	contactTpl := views.Must(views.ParseFS(templates.FS, "base.gohtml", "contact.gohtml"))
 	r.Get("/contact", controllers.StaticHandler(contactTpl))
 
 	r.Get("/faq", controllers.FAQ(
